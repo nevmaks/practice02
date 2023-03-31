e247b9a720a890d9ed34f4354daf0972d9b8722a
Maksym Nevdashchenko <nevmaks@gmail.com>
2023-03-31 12:01:33 +0300

    Changed app for different content

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-
-const root = ReactDOM.createRoot(document.getElementById("app"));
-
-root.render(
-    React.createElement('h1', null, 'Hello, world!'),
+let app = React.createElement('div', {},
+    React.createElement('h1', {}, "Hi, I'm header inside div"),
+    React.createElement('ul', {},
+        React.createElement('li', {},
+            React.createElement('h2', {}, "Hi, I'm list item inside list inside div")
+        ),
+        React.createElement('li', {},
+            React.createElement('h2', {}, "Hi, I'm list item inside list inside div")
+        )
+    )
 );
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(app);
+
